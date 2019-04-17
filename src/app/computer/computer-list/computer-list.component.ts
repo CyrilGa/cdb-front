59cfc6eb739b5f433aa6add50832abc533e4184b
BertrandMEJEAN <bertrand.mejean@gmail.com>
2019-04-17 14:33:24 +0200

    [ADD] [Component]: add Mock

--- src/app/computer/computer-list/computer-list.component.ts
+++ src/app/computer/computer-list/computer-list.component.ts
@@ -5,7 +5,111 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './computer-list.component.html',
   styleUrls: ['./computer-list.component.scss']
 })
+
 export class ComputerListComponent implements OnInit {
+
+  Mock: [
+    {
+        'id': '18',
+        'name': 'COSMAC ELF',
+        'introduced': '',
+        'discontinued': '',
+        'companyDTO': {
+            'id': '3',
+            'name': 'RCA'
+        }
+    },
+    {
+        'id': '19',
+        'name': 'COSM',
+        'introduced': '1977-01-02 01:00',
+        'discontinued': '',
+        'companyDTO': {
+            'id': '3',
+            'name': 'RCA'
+        }
+    },
+    {
+        'id': '20',
+        'name': 'ELF II',
+        'introduced': '1977-01-01 01:00',
+        'discontinued': '',
+        'companyDTO': {
+            'id': '4',
+            'name': 'Netronics'
+        }
+    },
+    {
+        'id': '22',
+        'name': 'Macintosh II',
+        'introduced': '',
+        'discontinued': '',
+        'companyDTO': {
+            'id': null,
+            'name': null
+        }
+    },
+    {
+        'id': '24',
+        'name': 'Macintosh IIfx',
+        'introduced': '',
+        'discontinued': '',
+        'companyDTO': {
+            'id': null,
+            'name': null
+        }
+    },
+    {
+        'id': '30',
+        'name': 'Xserve',
+        'introduced': '',
+        'discontinued': '',
+        'companyDTO': {
+            'id': null,
+            'name': null
+        }
+    },
+    {
+        'id': '31',
+        'name': 'Powerbook 100',
+        'introduced': '',
+        'discontinued': '',
+        'companyDTO': {
+            'id': null,
+            'name': null
+        }
+    },
+    {
+        'id': '32',
+        'name': 'Powerbook 140',
+        'introduced': '',
+        'discontinued': '',
+        'companyDTO': {
+            'id': null,
+            'name': null
+        }
+    },
+    {
+        'id': '33',
+        'name': 'Powerbook 170',
+        'introduced': '',
+        'discontinued': '',
+        'companyDTO': {
+            'id': null,
+            'name': null
+        }
+    },
+    {
+        'id': '34',
+        'name': 'PowerBook Duo',
+        'introduced': '',
+        'discontinued': '',
+        'companyDTO': {
+            'id': null,
+            'name': null
+        }
+    }
+  ];
 
   constructor() { }
 
--- src/app/computer/computer.module.ts
+++ src/app/computer/computer.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComputerListComponent } from './computer-list/computer-list.component';
-import { ComputerComponent } from './computer/computer.component';
-import { ComputerFormComponent } from './computer-form/computer-form.component';
+//import { ComputerComponent } from './computer/computer.component';
+//import { ComputerFormComponent } from './computer-form/computer-form.component';
 
 @NgModule({
   declarations: [
     ComputerListComponent,
-    ComputerComponent,
-    ComputerFormComponent
+    //ComputerComponent,
+    //ComputerFormComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ComputerListComponent,
-    ComputerComponent,
-    ComputerFormComponent
+    //ComputerComponent,
+    //ComputerFormComponent
   ]
 })
 export class ComputerModule { }
