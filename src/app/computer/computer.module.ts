deb916fccfbe2eb7e09cb499fe027382828c8c36
CyrilGa <cgaiton@excilys.com>
2019-04-17 10:55:07 +0200

    fix computer-module names

--- src/app/computer/computer.module.ts
+++ src/app/computer/computer.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComputerList } from './computer-list/computer-list.component';
-import { Computer } from './computer/computer.component';
-import { ComputerForm } from './computer-form/computer-form.component';
+import { ComputerListComponent } from './computer-list/computer-list.component';
+import { ComputerComponent } from './computer/computer.component';
+import { ComputerFormComponent } from './computer-form/computer-form.component';
 
 @NgModule({
   declarations: [
-    ComputerList,
-    Computer,
-    ComputerForm
+    ComputerListComponent,
+    ComputerComponent,
+    ComputerFormComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ComputerList,
-    Computer,
-    ComputerForm
+    ComputerListComponent,
+    ComputerComponent,
+    ComputerFormComponent
   ]
 })
 export class ComputerModule { }
