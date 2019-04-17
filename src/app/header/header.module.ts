1d75b3e72aa3c579774d5568045c0fe90563cc83
Clem <clementm.nerdy@gmail.com>
2019-04-17 15:09:04 +0200

    Added some style on Relative Header

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderRelativeComponent } from './header-relative/header-relative.component';
 import { HeaderFixComponent } from './header-fix/header-fix.component';
+import { HeaderComponent } from './header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatButtonModule } from '@angular/material/button'; 
 
 @NgModule({
-  declarations: [HeaderRelativeComponent, HeaderFixComponent],
+  declarations: [HeaderRelativeComponent, HeaderFixComponent, HeaderComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports: [HeaderComponent]
 })
 export class HeaderModule { }
