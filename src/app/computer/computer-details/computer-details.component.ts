ed33d2bdbca80201e4752c33bf2498a6cc7e2760
BertrandMEJEAN <bertrand.mejean@gmail.com>
2019-04-18 09:59:33 +0200

    [ADD][computer-list]: add call to computer-details

--- src/app/computer/computer-details/computer-details.component.ts
+++ src/app/computer/computer-details/computer-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input } from '@angular/core';
 import {Computer} from '../model/computer.model';
 
 @Component({
@@ -6,14 +6,10 @@ import {Computer} from '../model/computer.model';
   templateUrl: './computer-details.component.html',
   styleUrls: ['./computer-details.component.scss']
 })
-export class ComputerDetailsComponent implements OnInit {
+export class ComputerDetailsComponent{
 
-  // @Input computer: Computer;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input()
+  computer: Computer;
 
   deleteComputer(computer) {
 
