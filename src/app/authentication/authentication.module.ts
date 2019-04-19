9e9967e44260e451dcb8f6f11f82e8da5ae789b0
Remi Momprive <remi.momprive@gmail.com>
2019-04-19 11:08:10 +0200

    [Refacto] Add custom material module

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
-import { MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import {CustomMaterialModule} from '../custom-material/custom-material.module';
 
 @NgModule({
   declarations: [
@@ -9,9 +9,7 @@ import { MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/ma
   ],
   imports: [
     CommonModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule
+    CustomMaterialModule
   ],
   exports: [
     LoginFormComponent
