238d14a4315ded717d58da80cb25a0076cb04fe8
Remi Momprive <remi.momprive@gmail.com>
2019-04-24 12:14:52 +0200

    Add snackbars for error and info

--- src/app/authentication/login-form/login-form.component.ts
+++ src/app/authentication/login-form/login-form.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit, Output} from '@angular/core';
 import {ApiService} from '../../service/api.service';
 import {Router} from '@angular/router';
 import {SessionService} from '../session.service';
+import {MatSnackBar} from '@angular/material';
+import {SnackbarComponent} from '../../custom-material/snackbar/snackbar.component';
 
 @Component({
   selector: 'app-login-form',
@@ -12,7 +14,10 @@ export class LoginFormComponent implements OnInit {
   username = '';
   password = '';
 
-  constructor(private apiService: ApiService, private sessionService: SessionService, private router: Router) {
+  constructor(private snackBar: MatSnackBar,
+              private apiService: ApiService,
+              private sessionService: SessionService,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -25,7 +30,14 @@ export class LoginFormComponent implements OnInit {
     this.apiService.login(this.username, this.password).subscribe(response => {
       this.sessionService.saveUser(response);
       this.router.navigate(['/']);
-    }, error => this.sessionService.invalidateUser());
-  }
+    }, error => {
+      this.sessionService.invalidateUser();
 
+      this.snackBar.openFromComponent(SnackbarComponent, {
+        duration: 5 * 1000,
+        panelClass: ['snackbar-error'],
+        data: 'Incorrect login/password'
+      });
+    });
+  }
 }
--- src/app/authentication/register-form/register-form.component.ts
+++ src/app/authentication/register-form/register-form.component.ts
@@ -2,6 +2,9 @@ import {Component, Input, OnInit, Output} from '@angular/core';
 import {ApiService} from '../../service/api.service';
 import {Router} from '@angular/router';
 import {SessionService} from '../session.service';
+import {SnackbarComponent} from '../../custom-material/snackbar/snackbar.component';
+import {SuccessSnackbarComponent} from '../../custom-material/success-snackbar/success-snackbar.component';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-register-form',
@@ -12,9 +15,13 @@ export class RegisterFormComponent implements OnInit {
   username = '';
   password = '';
   passwordRepeat = '';
-  canRegister = false;
+  goodUsernameLength = false;
+  passwordMatch = false;
 
-  constructor(private apiService: ApiService, private sessionService: SessionService, private router: Router) {
+  constructor(private snackBar: MatSnackBar,
+              private apiService: ApiService,
+              private sessionService: SessionService,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -25,23 +32,52 @@ export class RegisterFormComponent implements OnInit {
 
   register() {
     if (this.password === this.passwordRepeat) {
-      this.apiService.register(this.username, this.password).subscribe(response => alert('success'));
+      this.apiService.register(this.username, this.password).subscribe(response => {
+          this.snackBar.openFromComponent(SnackbarComponent, {
+            duration: 5 * 1000,
+            panelClass: ['snackbar', 'snackbar-success'],
+            data: 'Successfully registered'
+          });
+
+          this.username = '';
+          this.password = '';
+          this.passwordRepeat = '';
+        },
+        error => {
+          this.snackBar.openFromComponent(SnackbarComponent, {
+            duration: 5 * 1000,
+            panelClass: ['snackbar', 'snackbar-error'],
+            data: 'An error happened'
+          });
+        });
+    }
+  }
+
+  onUsernameInputChange($event) {
+    if ($event.length > 0) {
+      this.goodUsernameLength = true;
+    } else {
+      this.goodUsernameLength = false;
     }
   }
 
   onPasswordInputChange($event) {
     if ($event === this.passwordRepeat) {
-      this.canRegister = true;
+      this.passwordMatch = true;
     } else {
-      this.canRegister = false;
+      this.passwordMatch = false;
     }
   }
 
   onPasswordRepeatInputChange($event) {
     if (this.password === $event) {
-      this.canRegister = true;
+      this.passwordMatch = true;
     } else {
-      this.canRegister = false;
+      this.passwordMatch = false;
     }
+  }
+
+  canRegister() {
+    return this.goodUsernameLength && this.passwordMatch;
   }
 }
